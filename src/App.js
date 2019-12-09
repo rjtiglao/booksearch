c51cfe4775b134cbf4cf9464a964c6ad6a18439e
Rj Tiglao <rjtiglao@gmail.com>
2019-12-08 20:54:52 -0800

    index change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Nav from "./component/Nav/Nav";
-import Home from "./component/pages/Home";
+import Home from "./component/Views/Home/Home";
 import SavedBooks from "./component/saved/SavedBooks";
 import SBooksState from "./component/context/SavedBooks/SBooksState";
 import GBooksState from "./component/context/GoogleBooks/GBooksState";
