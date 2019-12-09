627625b60004139f6422293db06578f3a1ba138f
Rj Tiglao <rjtiglao@gmail.com>
2019-12-08 20:52:25 -0800

    made a few changes in books and views

---
+++ src/component/Views/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Header from "../../Header/Header";
+import Books from "../../Books/Books";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Books />
+    </Fragment>
+  );
+};
+
+export default Home;
